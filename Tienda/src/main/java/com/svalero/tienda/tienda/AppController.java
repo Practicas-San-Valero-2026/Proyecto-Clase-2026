e7164e6f019464f6cadcea447f97824a0686e3af
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-06 17:57:52 +0100

    Modificacion 3

--- Tienda/src/main/java/com/svalero/tienda/tienda/AppController.java
+++ Tienda/src/main/java/com/svalero/tienda/tienda/AppController.java
@@ -1,12 +1,15 @@
 package com.svalero.tienda.tienda;
 
+import com.svalero.tienda.tienda.dao.ProductoDAO;
 import com.svalero.tienda.tienda.model.Clientes;
 import com.svalero.tienda.tienda.model.Pedidos;
 import com.svalero.tienda.tienda.model.Productos;
 import com.svalero.tienda.tienda.model.Vista;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +46,8 @@ public class AppController {
     private TableColumn<Vista,String> vEmail;
     @FXML
     private TableColumn<Vista,String> vTelefono;
+    @FXML
+    private Label statusLabel;
 
     private List<Productos> productosList;
     @FXML
@@ -112,6 +117,7 @@ public class AppController {
     private ListView cListView;
 
 
+    // BOTONES PRODUCTOS
     @FXML
     protected void nuevoProducto(ActionEvent event) {
 
@@ -129,12 +135,85 @@ public class AppController {
     @FXML
     protected void guardarProducto(ActionEvent event) {
 
+        if (pNombreField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("El nombre del producto es un campo obligatorio");
+            alert.show();
+            return;
+        }
 
+        String nombre = pNombreField.getText();
+        String tipo = pTipoField.getText();
+        float precio;
+        try {
+            precio = Float.parseFloat(pPrecioField.getText());
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("El precio debe ser un número válido");
+            alert.show();
+            return;
+        }
 
+        boolean stock = pStockCheckBox.isSelected();
+        String descripcion = pDescripcionTArea.getText();
+
+        int id = Integer.parseInt(pPrecioField.getText());
+        Productos producto = new Productos(id, nombre, tipo, precio, stock, descripcion);
+        productosList.add(producto);
+        showStatus("Producto añadido correctamente", 5);
+
+        // Añadir BD (DAO)
+        ProductoDAO dao = new ProductoDAO();
+        dao.insert(producto);
+
+        desactivarCamposProductos();
+
+        pGuardarButton.setDisable(true);
+        pNuevoButton.setDisable(false);
+        pModificarButton.setDisable(false);
+        limpiarCamposProductos();
+
+        refreshProductos();
     }
+
 
     @FXML
     protected void modificarProducto(ActionEvent event) {
+
+        pModificarButton.setDisable(false);
+        Productos selected = (Productos) pListView.getSelectionModel().getSelectedItem();
+        if (selected == null) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Selecciona un producto de la tabla para modificarlo");
+            alert.show();
+            return;
+        }
+
+        if (pNombreField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("El nombre del producto es obligatorio");
+            alert.show();
+            return;
+        }
+
+        float precio;
+        try {
+            precio = Float.parseFloat(pPrecioField.getText());
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("El precio debe ser un número válido");
+            alert.show();
+            return;
+        }
+
+        selected.setNombre(pNombreField.getText());
+        selected.setTipo(pTipoField.getText());
+        selected.setPrecio(Float.parseFloat(pPrecioField.getText()));
+        selected.setStock(pStockCheckBox.isSelected());
+        selected.setDescripcion(pDescripcionTArea.getText());
+
+        productosList.reversed();
+        showStatus("Producto modificado correctamente", 5);
 
     }
 
@@ -152,17 +231,35 @@ public class AppController {
         pDescripcionTArea.setEditable(true);
     }
 
+    // desactivar campos de productos
+    private void desactivarCamposProductos() {
+        pNombreField.setEditable(false);
+        pTipoField.setEditable(false);
+        pPrecioField.setEditable(false);
+        pStockCheckBox.setDisable(true);
+        pDescripcionTArea.setDisable(true);
+    }
+
     // Limpiar campos de productos
     private void limpiarCamposProductos() {
         pNombreField.setText("");
         pPrecioField.setText("");
         pTipoField.setText("");
+        pStockCheckBox.setSelected(false);
         pDescripcionTArea.setText("");
 
     }
 
+    private void refreshProductos() {
+        pListView.getItems().clear();
+        for (Productos productos : productosList) {
+            pListView.getItems().add(productos.getNombre());
+        }
+    }
 
 
+
+    // BOTONES PEDIDOS
     @FXML
     protected void nuevoPedido(ActionEvent event) {
 
@@ -185,6 +282,7 @@ public class AppController {
 
 
 
+    // BOTONES CLIENTES
     @FXML
     protected void nuevoCliente(ActionEvent event) {
 
@@ -204,5 +302,14 @@ public class AppController {
     protected void eliminarCliente(ActionEvent event) {
 
     }
+
+    // Limpiando la statusBar después de x segundos
+    private void showStatus(String text, double seconds) {
+    statusLabel.setText(text);
+
+    PauseTransition pause = new PauseTransition(Duration.seconds(seconds));
+    pause.setOnFinished(e -> statusLabel.setText(""));
+    pause.play();
+}
 
 }
